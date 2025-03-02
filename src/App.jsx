7314d47ed6f6fcd56f80e37c802d3da539f2b240
Matheus Meigre <matheusmeigre@hotmail.com>
2025-03-02 20:31:53 -0300

    change App.jsx to normal code without structure changeTheme

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,9 @@ import Service from './components/Service'
 import Footer from './components/Footer'
 
 function App() {
-  const [theme, setTheme] = useState(() => {
-    return localStorage.getItem("theme") || "light"; // Busca o tema salvo ou define "light" por padrão
-  });
-
-  useEffect(() => {
-    // Define a classe "dark" no <html> ou <body> baseado no tema
-    if (theme === "dark") {
-      document.getElementById("root").classList.add("dark");
-    } else {
-      document.getElementById("root").classList.remove("dark");
-    }
-    // Salva a preferência no localStorage
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
   return (
     <div>
-      <Navbar  toggleTheme={toggleTheme} theme={theme}/>
+      <Navbar />
       <Hero />
       <About />
       <Service />
